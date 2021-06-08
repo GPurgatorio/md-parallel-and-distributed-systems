4717359a9fedf63194f36fcdc791dd41d6d9b87c
Giulio Purgatorio <38554644+GPurgatorio@users.noreply.github.com>
2021-06-08 10:22:31 +0200

    comments

--- utils/maxheap.cpp
+++ utils/maxheap.cpp
@@ -16,56 +16,43 @@
 
 using namespace std;
 
-// Min Heap Class
-// arr holds reference to an integer
-// array size indicate the number of
-// elements in Min Heap
 class MaxHeap {
 
     int size;
     vector<pair<int, float>> *arr;
 
 public:
-    // Constructor to initialize the size and arr
+
     MaxHeap(int size, vector<pair<int, float>> &input);
 
-    // Min Heapify function, that assumes that
-    // 2*i+1 and 2*i+2 are min heap and fix the
-    // heap property for i.
     void heapify(int i);
 
-    // Build the min heap, by calling heapify
-    // for all non-leaf nodes.
+    // Build the max heap, by calling heapify for all non-leaf nodes.
     void buildHeap();
 
+    // Tries to insert the given element into the maxheap
     void insertElem(pair<int, float> e);
 };
 
-// Constructor to initialize data
-// members and creating mean heap
-MaxHeap::MaxHeap(int size, vector<pair<int, float>> &input)
-{
-    // Initializing arr and size
 
+// Constructor
+MaxHeap::MaxHeap(int size, vector<pair<int, float>> &input) {
+    // Initializing arr and size
     this->size = size;
     this->arr = &input;
 
-    // Building the Min Heap
+    // Building the Max Heap
     buildHeap();
 }
 
-// Min Heapify function, that assumes
-// 2*i+1 and 2*i+2 are min heap and
-// fix min heap property for i
 
-void MaxHeap::heapify(int i)
-{
+// Max Heapify function
+void MaxHeap::heapify(int i) {
     // If Leaf Node, Simply return
     if (i >= size / 2)
         return;
 
-    // variable to store the smallest element
-    // index out of i, 2*i+1 and 2*i+2
+    // variable to store the smallest (i, left, right)
     float smallest;
 
     // Index of left node
@@ -74,38 +61,33 @@ void MaxHeap::heapify(int i)
     // Index of right node
     int right = 2 * i + 2;
 
-    // Select minimum from left node and
-    // current node i, and store the minimum
-    // index in smallest variable
+    // Store minimum from left node and current node i
     smallest = (*arr)[left].second > (*arr)[i].second ? left : i;
 
-    // If right child exist, compare and
-    // update the smallest variable
+    // If right child exist, compare and update
     if (right < size)
         smallest = (*arr)[right].second > (*arr)[smallest].second
                              ? right : smallest;
 
-    // If Node i violates the min heap
-    // property, swap  current node i with
-    // smallest to fix the min-heap property
-    // and recursively call heapify for node smallest.
+    // If Node i violates the max heap property, swap current node i with
+    // smallest and recursively call heapify for node smallest.
     if (smallest != i) {
         swap((*arr)[i], (*arr)[smallest]);
         heapify(smallest);
     }
 }
 
-// Build Min Heap
-void MaxHeap::buildHeap()
-{
+
+// Build Max Heap
+void MaxHeap::buildHeap() {
     // Calling Heapify for all non leaf nodes
     for (int i = size / 2 - 1; i >= 0; i--) {
         heapify(i);
     }
 }
 
+// Tries to insert an elem by comparing it to the root
 void MaxHeap::insertElem(pair<int, float> elem) {
-  // Vai al contenuto di ARR e poi prendi il K-esimo elem
   if (elem.second < (*arr)[0].second) {
     (*arr)[0] = elem;
     heapify(0);
